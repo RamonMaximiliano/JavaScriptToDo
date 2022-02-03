c116b6242bd6ad4826a51ca8d58df7df99261540
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-02-02 22:26:32 -0300

    adding items

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function addItem(){
 
     let item = document.querySelector("input.inputText").value
-    console.log(item)
+
+/*     Normal replace =   let endItem = item.replace(" ","")
+       Multiple characters replace, separa por pipes cada coisa a ser replaced =  let endItem = item.replace(/ |  |   |    |     |      /g,''); 
+ */
+
+    let endItem = item.replace(/ |  |   |    |     |      |       |        |         |          /g,''); 
+    console.log(endItem)
 
     //criando div do item
     let newItem = document.createElement("div")
     newItem.setAttribute("class", "newItem")
-    newItem.setAttribute("id", item)
+    newItem.setAttribute("data-id", endItem)
 
     //criando paragraph
     let newItemParagraph = document.createElement("p")
@@ -14,21 +20,27 @@ function addItem(){
     let itemContent = document.createTextNode(item)
     newItemParagraph.appendChild(itemContent)
     
+    //Adicionando o item ao HTML
     document.querySelector(".itemList").appendChild(newItem)
 
-    document.querySelector(".newItem").appendChild(newItemParagraph)
+    document.querySelector(`[data-id=${endItem}]`).appendChild(newItemParagraph)
 
     let exclude = document.createElement("img")
     exclude.setAttribute("src", "./images/delete.png")
     exclude.setAttribute("class", "excludeButton")
-    document.querySelector(".newItem").appendChild(exclude)
+    //é importante por o this.id como parametro na função já caso contrario não encontra o ID quando chama a função
+    exclude.setAttribute("onclick", "excludeItem(this.id)")
+    exclude.setAttribute("id", endItem)
+    document.querySelector(`[data-id=${endItem}]`).appendChild(exclude)
+}
 
-    
-
+function excludeItem(event){
+    //o this.id já foi incluido direto como  parametro na hora de chamar função
+    let itemExclude = document.querySelector(`[data-id=${event}]`)
+    console.log(event)
+    itemExclude.remove()
 
 }
 
-// setar um Id baseado no texto, adicionar items baseados no id, e se o id já existir avisar
-
-
+//o querySelector somente aceita characters como identifiers, ele não aceita numeros no inicio, a class tem que começar com um character não numero 
 
