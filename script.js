ad7a6325b2c84da6b7c2250777507eed3c231465
Ramon <ramon_max1986@hotmail.com>
2022-02-04 17:02:24 -0300

    almost

--- script.js
+++ script.js
@@ -1,61 +1,65 @@
 let itemsArray = []
 
-function addItem(){
-
+function addItem() {
     let item = document.querySelector("input.inputText").value
+    /*     Normal replace =   let endItem = item.replace(" ","")
+           Multiple characters replace, separa por pipes cada coisa a ser replaced =  let endItem = item.replace(/ |  |   |    |     |      /g,''); 
+     */
 
-/*     Normal replace =   let endItem = item.replace(" ","")
-       Multiple characters replace, separa por pipes cada coisa a ser replaced =  let endItem = item.replace(/ |  |   |    |     |      /g,''); 
- */
+    if (item != "") {
 
-    if(item != ""){
-       
-    let endItem = item.replace(/ |  |   |    |     |      |       |        |         |          /g,''); 
-    console.log(endItem)
+        let endItem = item.replace(/ |  |   |    |     |      |       |        |         |          /g, '');
+        console.log(endItem)
 
-    //validando se o item já existe no array
-    let checkExists = itemsArray.indexOf(endItem)
-    if(checkExists > -1){
-        window.alert("This item already exists, provide another item")
+        let checkNumbers = [...endItem]
+        console.log(checkNumbers[0])
+        let firstNumber = Number(checkNumbers[0])
+        console.log(firstNumber)
+
+        if (firstNumber == 0 || firstNumber == 1 || firstNumber == 2 || firstNumber == 3 || firstNumber == 4 || firstNumber == 5 || firstNumber == 6 || firstNumber == 7 || firstNumber == 8 || firstNumber == 9) {
+            window.alert("Do not start your item with a number, it needs to be a letter!")
+        } else {
+
+            //validando se o item já existe no array
+            let checkExists = itemsArray.indexOf(endItem)
+            if (checkExists > -1) {
+                window.alert("This item already exists, provide another item")
+            } else {
+
+                //criando div do item
+                let newItem = document.createElement("div")
+                newItem.setAttribute("class", "newItem")
+                newItem.setAttribute("data-id", endItem)
+
+                //criando paragraph
+                let newItemParagraph = document.createElement("p")
+                newItemParagraph.setAttribute("class", "paragraph")
+                let itemContent = document.createTextNode(item)
+                newItemParagraph.appendChild(itemContent)
+
+                //Adicionando o item ao HTML
+                document.querySelector(".itemList").appendChild(newItem)
+                document.querySelector(`[data-id=${endItem}]`).appendChild(newItemParagraph)
+
+                let exclude = document.createElement("img")
+                exclude.setAttribute("src", "./images/delete.png")
+                exclude.setAttribute("class", "excludeButton")
+                //é importante por o this.id como parametro na função já caso contrario não encontra o ID quando chama a função
+                exclude.setAttribute("onclick", "excludeItem(this.id)")
+                exclude.setAttribute("id", endItem)
+                document.querySelector(`[data-id=${endItem}]`).appendChild(exclude)
+
+                //adding new item to the array    
+                itemsArray.push(endItem)
+            }
+        }
     } else {
-
-    //criando div do item
-    let newItem = document.createElement("div")
-    newItem.setAttribute("class", "newItem")
-    newItem.setAttribute("data-id", endItem)
-
-    //criando paragraph
-    let newItemParagraph = document.createElement("p")
-    newItemParagraph.setAttribute("class", "paragraph")
-    let itemContent = document.createTextNode(item)
-    newItemParagraph.appendChild(itemContent)
-    
-    //Adicionando o item ao HTML
-    document.querySelector(".itemList").appendChild(newItem)
-
-    document.querySelector(`[data-id=${endItem}]`).appendChild(newItemParagraph)
-
-    let exclude = document.createElement("img")
-    exclude.setAttribute("src", "./images/delete.png")
-    exclude.setAttribute("class", "excludeButton")
-    //é importante por o this.id como parametro na função já caso contrario não encontra o ID quando chama a função
-    exclude.setAttribute("onclick", "excludeItem(this.id)")
-    exclude.setAttribute("id", endItem)
-    document.querySelector(`[data-id=${endItem}]`).appendChild(exclude)
-
-
-    //adding new item to the array    
-    itemsArray.push(endItem)
+        window.alert("We do not accept empty items in the list!")
     }
-
-} else {
-    window.alert("We do not accept empty items in the list!")
+    console.log(itemsArray)
 }
 
-console.log(itemsArray)
-}
-
-function excludeItem(event){
+function excludeItem(event) {
     //o this.id já foi incluido direto como  parametro na hora de chamar função
     let itemExclude = document.querySelector(`[data-id=${event}]`)
     console.log(event)
@@ -67,11 +71,14 @@ function excludeItem(event){
     Este splice method ta dizendo que na position do elemento index, remover 1 item, ou seja acaba deletando ele mesmo */
 
     let indexOfItem = itemsArray.indexOf(event)
-    if (indexOfItem >-1){
-        itemsArray.splice(indexOfItem,1)
-}
-console.log(itemsArray)
+    if (indexOfItem > -1) {
+        itemsArray.splice(indexOfItem, 1)
+    }
+    console.log(itemsArray)
 }
 
 //o querySelector somente aceita characters como identifiers, ele não aceita numeros no inicio, a class tem que começar com um character não numero 
 
+function reset(){
+    window.location.reload();
+}
